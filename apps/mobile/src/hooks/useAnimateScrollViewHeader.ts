5d3074e3fa2b507f13e3d7d7c538ea10b0d61487
Luan Pham <pqkluan@gmail.com>
2024-10-19 00:27:12 +0700

    feat: improve performance of animate hook

--- apps/mobile/src/hooks/useAnimateScrollViewHeader.ts
+++ apps/mobile/src/hooks/useAnimateScrollViewHeader.ts
@@ -22,49 +22,57 @@ export const useAnimateScrollViewHeader = (params: Params) => {
 	const scrollOffset = useScrollViewOffset(scrollRef);
 
 	const lastOffset = useSharedValue(0);
-	const lastDerivedValue = useSharedValue(0);
+	const lastOffsetDistance = useSharedValue(0);
 	const lastMinOffSet = useSharedValue(0);
 	const lastMaxOffSet = useSharedValue(animateDistance);
 
 	const offsetDistance = useDerivedValue(() => {
 		const diff = scrollOffset.value - lastOffset.value;
 
-		if (diff === 0) {
-			// Case 1: The new offset is the same as the last offset for some reason
-			// Just return the last derived value
-			return lastDerivedValue.value;
-		}
-
 		if (diff > 0) {
-			// Case 2: Scrolling down
+			// Case 1: Scrolling down
 
-			// Update the max offset if user scroll past it
+			// Update the max offset if user scroll down past it
 			if (scrollOffset.value > lastMaxOffSet.value) {
 				// Max offset need to be at least the animate distance
-				lastMaxOffSet.value = Math.max(scrollOffset.value, animateDistance);
-				// We also need to drag the min offset along with the new max offset
-				lastMinOffSet.value = Math.max(scrollOffset.value - animateDistance, 0);
+				const maxOffset = Math.max(scrollOffset.value, animateDistance);
+				if (lastMaxOffSet.value !== maxOffset) lastMaxOffSet.value = maxOffset;
+
+				// We also need to pull the min offset along with the new max offset
+				const minOffset = Math.max(scrollOffset.value - animateDistance, 0);
+				if (lastMinOffSet.value !== minOffset) lastMinOffSet.value = minOffset;
 			}
 		} else if (diff < 0) {
-			// Case 3: Scrolling up
+			// Case 2: Scrolling up
 
-			// Update the min offset if the current offset is less than the last min offset
+			// Update the min offset if user scroll up past it
 			if (scrollOffset.value < lastMinOffSet.value) {
 				// Limit the min offset to be at least at 0
-				lastMinOffSet.value = Math.max(scrollOffset.value, 0);
-				// Update the max offset after the min offset
-				lastMaxOffSet.value = Math.max(lastMinOffSet.value + animateDistance, animateDistance);
+				const minOffset = Math.max(scrollOffset.value, 0);
+				if (lastMinOffSet.value !== minOffset) lastMinOffSet.value = minOffset;
+
+				// We also need to lower the max offset along with the new min offset
+				const maxOffset = Math.max(lastMinOffSet.value + animateDistance, animateDistance);
+				if (lastMaxOffSet.value !== maxOffset) lastMaxOffSet.value = maxOffset;
 			}
+		}
+
+		if (diff === 0) {
+			// Case 3: The new offset is the same as the last offset for some reason
+			// Just return the last derived value
+			return lastOffsetDistance.value;
 		}
 
 		// Calculate the amount of distance from the current offset to the last min offset
 		// Also limit the derived value to be between 0 and animate distance
-		lastDerivedValue.value = clamp(scrollOffset.value - lastMinOffSet.value, 0, animateDistance);
+		const nextOffsetDistance = clamp(scrollOffset.value - lastMinOffSet.value, 0, animateDistance);
+		if (lastOffsetDistance.value !== nextOffsetDistance)
+			lastOffsetDistance.value = nextOffsetDistance;
 
-		// Always update the last offset at the end
+		// Update the last offset at the end
 		lastOffset.value = scrollOffset.value;
 
-		return lastDerivedValue.value;
+		return nextOffsetDistance;
 	});
 
 	const headerStyle = useAnimatedStyle(() => ({
